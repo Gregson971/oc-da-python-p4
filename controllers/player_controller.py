6086571619c1f1513552bb066db6fe8b87693537
Grégory Lama <gregory.lama@gmail.com>
2023-11-01 22:56:54 +0100

    refactor(chess game manager): refactor create_player fonction by moving the validation of birthdate and chess_id

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -20,34 +20,8 @@ class PlayerController:
         player_informations = self.view.create_player()
         first_name = player_informations["first_name"]
         last_name = player_informations["last_name"]
-
-        birthdate = None
-        while not birthdate:
-            birthdate_string = player_informations["birthdate"]
-            try:
-                datetime.strptime(birthdate_string, "%d-%m-%Y")
-                birthdate = birthdate_string
-                if len(birthdate) != 10 or birthdate[2] != "-" or birthdate[5] != "-":
-                    self.view.display_message(message="Invalid birthdate format. Please use DD-MM-YYYY format.")
-                    birthdate = None
-            except ValueError:
-                self.view.display_message(message="Please enter a valid date in format DD-MM-YYYY.")
-                break
-
-        chess_id = None
-        while not chess_id:
-            chess_id_str = player_informations["chess_id"]
-            if len(chess_id_str) != 7:
-                self.view.display_message(
-                    message="Invalid chess ID format. Please use 2 letters followed by 5 digits."
-                )
-                break
-            if not chess_id_str[:2].isalpha() or not chess_id_str[2:].isdigit():
-                self.view.display_message(
-                    message="Invalid chess ID format. Please use 2 letters followed by 5 digits."
-                )
-                break
-            chess_id = chess_id_str
+        birthdate = self.validate_birthdate(player_informations["birthdate"])
+        chess_id = self.validate_chess_id(player_informations["chess_id"])
 
         if not first_name or not last_name or not birthdate or not chess_id:
             self.view.display_message(message="Please enter valid informations.")
@@ -57,6 +31,34 @@ class PlayerController:
         self.tournament.players.append(new_player)
 
         PlayerService.save_player(new_player)
+
+    def validate_birthdate(self, birthdate_string):
+        """Validate birthdate"""
+        while True:
+            try:
+                datetime.strptime(birthdate_string, "%d-%m-%Y")
+                if len(birthdate_string) != 10 or birthdate_string[2] != "-" or birthdate_string[5] != "-":
+                    self.view.display_message(message="Invalid birthdate format. Please use DD-MM-YYYY format.")
+                else:
+                    return birthdate_string
+            except ValueError:
+                self.view.display_message(message="Please enter a valid date in format DD-MM-YYYY.")
+                return None
+
+    def validate_chess_id(self, chess_id_str):
+        """Validate chess ID"""
+        while True:
+            if len(chess_id_str) != 7:
+                self.view.display_message(
+                    message="Invalid chess ID format. Please use 2 letters followed by 5 digits."
+                )
+                return None
+            if not chess_id_str[:2].isalpha() or not chess_id_str[2:].isdigit():
+                self.view.display_message(
+                    message="Invalid chess ID format. Please use 2 letters followed by 5 digits."
+                )
+                return None
+            return chess_id_str
 
     def load_players(self):
         """Load players from the database"""
